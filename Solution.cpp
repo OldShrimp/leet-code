0678f0c00e0768425a1b10f7676a048d9091ebd9
Tyler Nex <tylernex@gmail.com>
2024-09-03 11:36:09 -0700

    added solution

--- Solution.cpp
+++ Solution.cpp
@@ -1,28 +1,19 @@
-// https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// https://leetcode.com/problems/fizz-buzz
 class Solution {
 public:
-
-    TreeNode* prev = nullptr;
-    void flatten(TreeNode* root)
-    {
-        if (root != nullptr) {
-            flatten(root->right);
-            flatten(root->left);
-
-            root->right = prev;
-            root->left = nullptr;
-            prev = root;
+    vector<string> fizzBuzz(int n) {
+        vector<string> answer(n, "");
+        for (int i = 1; i <= n; i++) {
+            bool isFizz = (i % 3) == 0;
+            bool isBuzz = (i % 5) == 0;
+            if (!isFizz && !isBuzz)
+                answer[i - 1] = to_string(i);
+            else {
+                string fizz[] = { "", "Fizz" };
+                string buzz[] = { "", "Buzz" };
+                answer[i - 1] = fizz[isFizz] + buzz[isBuzz];
+            }
         }
+        return answer;
     }
 };
