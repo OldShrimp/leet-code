0c5662ba029e8692ef5af27345eb9b1305786874
Tyler Nex <tylernex@gmail.com>
2024-09-03 11:19:09 -0700

    added solution

--- Solution.cpp
+++ Solution.cpp
@@ -1,14 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        int longest_substring = 0;
-        for (auto i = s.begin(); i != s.end(); i++) {
-            auto subStringStart = i, subStringEnd = i;
-            while (subStringEnd != s.end() && find(subStringStart, subStringEnd, *(subStringEnd)) == subStringEnd) {
-                subStringEnd++;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* sum = new ListNode();
+        ListNode* sumCurrent = sum;
+        while (l1 || l2) {
+            int n1 = 0, n2 = 0;
+            if (l1) {
+                sumCurrent->val += l1->val;
+                l1 = l1->next;
             }
-            longest_substring = max(longest_substring, static_cast<int>(distance(subStringStart, subStringEnd)));
+            if (l2) {
+                sumCurrent->val += l2->val;
+                l2 = l2->next;
+            }
+            if (sumCurrent->val > 9) {
+                sumCurrent->val -= 10;
+                sumCurrent->next = new ListNode(1);
+            }
+            else
+                if (l1 || l2)
+                    sumCurrent->next = new ListNode();
+            sumCurrent = sumCurrent->next;
         }
-        return longest_substring;
+        return sum;
     }
 };
