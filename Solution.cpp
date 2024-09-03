8a25a4c0116ca4613be267a29adeb889baba5129
Tyler Nex <tylernex@gmail.com>
2024-09-03 11:30:40 -0700

    added solution

--- Solution.cpp
+++ Solution.cpp
@@ -1,12 +1,25 @@
-// https://leetcode.com/problems/sum-of-square-numbers
+// https://leetcode.com/problems/get-maximum-in-generated-array/
 class Solution {
 public:
-    bool judgeSquareSum(int c) {
-        for (unsigned int i = 0; i * i <= c; i++) {
-            auto x = sqrt(c - i * i);
-            if (trunc(x) == x)
-                return true;
+    int getMaximumGenerated(int n) {
+        if (n < 2)
+            return n;
+
+        int largest = 1;
+
+        vector<int> vec;
+        vec.reserve(n + 1);
+        vec.push_back(0);
+        vec.push_back(1);
+        for (int i = 2; i <= n; i++) {
+            if (i % 2 == 0) {
+                vec.push_back(vec[i / 2]);
+            }
+            else {
+                vec.push_back(vec[(i - 1) / 2] + vec[(i - 1) / 2 + 1]);
+            }
+            largest = max(largest, vec[i]);
         }
-        return false;
+        return largest;
     }
 };
