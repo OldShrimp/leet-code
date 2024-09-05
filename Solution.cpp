2575449cdb6fc08888ac57d5c682f3a88e281c48
Tyler Nex <tylernex@gmail.com>
2024-09-04 17:58:30 -0700

    added solution

--- Solution.cpp
+++ Solution.cpp
@@ -1,11 +1,36 @@
-// https://leetcode.com/problems/find-the-number-of-good-pairs-i
+// https://leetcode.com/problems/longest-palindromic-substring
 class Solution {
 public:
-    int numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        int goodPairs = 0;
-        for (auto& num1 : nums1)
-            for (auto& num2 : nums2)
-                goodPairs += static_cast<int>((num1 % (num2 * k)) == 0);
-        return goodPairs;
+    string longestPalindrome(string s) {
+        string longest_palindrome = "";
+        for (int i = 0; i < s.length(); i++) {
+            auto leftIter = s.begin() + i;
+            auto rightIter = leftIter;
+            while (*leftIter == *rightIter) {
+                string palindrome(leftIter, rightIter + 1);
+                if (palindrome.length() > longest_palindrome.length())
+                    longest_palindrome = palindrome;
+
+                if (leftIter == s.begin() || rightIter == s.end() - 1)
+                    break;
+                --leftIter;
+                ++rightIter;
+            }
+            leftIter = s.begin() + i;
+            rightIter = leftIter + 1;
+            if (rightIter != s.end()) {
+                while (*leftIter == *rightIter) {
+                    string palindrome(leftIter, rightIter + 1);
+                    if (palindrome.length() > longest_palindrome.length())
+                        longest_palindrome = palindrome;
+
+                    if (leftIter == s.begin() || rightIter == s.end() - 1)
+                        break;
+                    --leftIter;
+                    ++rightIter;
+                }
+            }
+        }
+        return longest_palindrome;
     }
 };
