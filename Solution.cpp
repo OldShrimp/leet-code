6853b8e261f62240e0b4863752503fb91d687850
Tyler Nex <tylernex@gmail.com>
2024-09-04 19:39:27 -0700

    added solution

--- Solution.cpp
+++ Solution.cpp
@@ -1,36 +1,34 @@
-// https://leetcode.com/problems/longest-palindromic-substring
+// https://leetcode.com/problems/zigzag-conversion/
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        string longest_palindrome = "";
-        for (int i = 0; i < s.length(); i++) {
-            auto leftIter = s.begin() + i;
-            auto rightIter = leftIter;
-            while (*leftIter == *rightIter) {
-                string palindrome(leftIter, rightIter + 1);
-                if (palindrome.length() > longest_palindrome.length())
-                    longest_palindrome = palindrome;
-
-                if (leftIter == s.begin() || rightIter == s.end() - 1)
-                    break;
-                --leftIter;
-                ++rightIter;
-            }
-            leftIter = s.begin() + i;
-            rightIter = leftIter + 1;
-            if (rightIter != s.end()) {
-                while (*leftIter == *rightIter) {
-                    string palindrome(leftIter, rightIter + 1);
-                    if (palindrome.length() > longest_palindrome.length())
-                        longest_palindrome = palindrome;
-
-                    if (leftIter == s.begin() || rightIter == s.end() - 1)
-                        break;
-                    --leftIter;
-                    ++rightIter;
+    string convert(string s, int numRows) {
+        if (numRows == 0)
+            return "";
+        if (numRows == 1)
+            return s;
+        vector<string> rows(numRows, string(""));
+        string spaced_str, spaces(numRows - 2, ' ');
+        if (numRows == 2) {
+            spaced_str = s;
+        }
+        else {
+            for (int i = 0; i < s.length(); i++) {
+                spaced_str += s[i];
+                if (i % (2 * numRows - 2) >= numRows - 1) {
+                    spaced_str += spaces;
                 }
             }
         }
-        return longest_palindrome;
+
+        for (int i = 0; i < spaced_str.length(); i++) {
+            rows[i % numRows] += spaced_str[i];
+        }
+        string converted = "";
+        for (auto& str : rows) {
+            cout << str << endl;
+            str.erase(remove(str.begin(), str.end(), ' '), str.end());
+            converted += str;
+        }
+        return converted;
     }
 };
